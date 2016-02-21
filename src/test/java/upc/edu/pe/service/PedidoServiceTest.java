373323381070e9d5bda9d73a08741b0255492c7d
yosuavirtual@gmail.com <yosuavirtual@gmail.com>
2016-02-21 12:10:14 -0500

    Modificacion del rest-Pedido

--- src/test/java/upc/edu/pe/service/PedidoServiceTest.java
+++ src/test/java/upc/edu/pe/service/PedidoServiceTest.java
@@ -46,11 +46,15 @@ public class PedidoServiceTest {
     /**
      * Test of listar method, of class PedidoService.
      */
-    //@Test
+    @Test
     public void testListar() throws Exception {
         System.out.println("listar");
         List<Pedido> result = instance.listar();
         System.out.println("Resultado : " + result.size());
+        for(Pedido ped : result){
+            System.out.println("Nombre : " + ped.getCliente().getNombre());
+            System.out.println("Apellidos : " + ped.getCliente().getApellidos());
+        }
         assertTrue(result != null);
     }
 
